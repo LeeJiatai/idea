7db8f2fc090e08169f4af3559fa23043816765e5
lijiatai <sunlinman>
2019-04-10 16:41:48 +0800

    :tada: 数据类型判断函数返回字符串为小写

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*获取数据类型*/
 let dataType = (data) => {
 	let string = Object.prototype.toString.call(data).slice();
-	let type = string.slice(string.indexOf(' ') + 1, string.length - 1);
+	let type = string.slice(string.indexOf(' ') + 1, string.length - 1).toLowerCase();
 	return type;
 }
