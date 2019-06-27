962bcea630046781560c376032c7f1c027dd2529
lijiatai <lijiataihpu@gmail.com>
2019-06-27 18:01:09 +0800

    push:pencil:

--- js/sort.js
+++ js/sort.js
@@ -1,8 +1,12 @@
-//冒泡排序
-function bubbleSort(array) {
-    if(array.length == 0) {
-        return array
+let checkLength = arr => {
+    if(arr.length == 0) {
+        return arr
     }
+}
+
+//冒泡排序
+let bubbleSort = array => {
+    checkLength(array)
 
     for ( let i = 0; i < array.length; i++ ) {
         for ( let j = 0; j < array.length - 1 - i; j++ ) {
@@ -11,6 +15,26 @@ function bubbleSort(array) {
                 array[j + 1] = array[j]
                 array[j] = temp 
             }
+        }
+    }
+
+    return array
+}
+
+//选择排序
+let selectionSort = array => {
+    checkLength(array)
+
+    for(let i = 0; i < array.length; i++) {
+        let minIndex = i
+        for (let j = i; j < array.length; j++) {
+            if(array[j] < array[minIndex]) {
+                minIndex = j
+            } 
+            let temp = array[minIndex]
+            array[minIndex] = array[i]
+            array[i] = temp
+            console.log(37, array)
         }
     }
 
