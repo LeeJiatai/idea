d5e0770d017b7043cd8bf358bac5f4ad5d64f623
lijiatai <lijiataihpu@gmail.com>
2019-06-30 17:34:22 +0800

    排序方法:zap:

--- js/sort.js
+++ js/sort.js
@@ -1,42 +1,79 @@
-let checkLength = arr => {
-    if(arr.length == 0) {
-        return arr
-    }
-}
-
 //冒泡排序
 let bubbleSort = array => {
-    checkLength(array)
-
-    for ( let i = 0; i < array.length; i++ ) {
-        for ( let j = 0; j < array.length - 1 - i; j++ ) {
-            if (array[j + 1] < array[j]) {
-                let temp = array[j + 1];
-                array[j + 1] = array[j]
-                array[j] = temp 
+    let len = array.length;
+    if (len <= 1) {
+        return array
+    }
+    for (let i = 0; i < len; i++) {
+        for(let j = 0; j < len - 1 - i; j++) {
+            if(array[j] > array[j+1]) {
+                [array[j+1], array[j]] = [array[j], array[j+1]]
             }
         }
     }
-
     return array
 }
 
 //选择排序
 let selectionSort = array => {
-    checkLength(array)
-
-    for(let i = 0; i < array.length; i++) {
-        let minIndex = i
-        for (let j = i; j < array.length; j++) {
-            if(array[j] < array[minIndex]) {
+    let len = array.length
+    let min, minIndex;
+    if(len <= 1) {
+        return array
+    }
+    for (let i = 0; i < len - 1; i++) {
+        min = array[i]
+        for (let j = i + 1; j < len; j++) {
+            if(array[j] < min) {
+                min = array[j]
                 minIndex = j
-            } 
-            let temp = array[minIndex]
-            array[minIndex] = array[i]
-            array[i] = temp
-            console.log(37, array)
+            }
+        }
+        if (min != array[i]) {
+            [array[minIndex], array[i]] = [array[i], array[minIndex]]
         }
     }
+    return array
+}
 
+//插入排序
+let insertSort = array => {
+    var len = array.length
+    if(len <= 1) {
+        return array
+    }
+	for (let i = 1; i < len; i++) {
+        let key = array[i]
+        let j = i - 1
+        while(j >= 0 && array[j] > key) {
+            array[j + 1] = array[j]
+            j--
+        }
+        array[j + 1] = key
+    }
+    return array
+}
+
+//希尔排序（缩小增量排序
+let shellSort = array => {
+    let len = array.length
+    let index = Math.floor(len / 2) //比较步长
+    let j, temp
+    console.log(56, array, index)
+    if(len <= 1) {
+        return array
+    }
+
+    while (index > 0){
+        for (var i = index; i < len; i++){//遍历起点后移，保证每个数在该步长下参与且只参与1此排序
+          temp = array[i];
+          for (j = i; j >= index && array[j - index] > temp;){//等步长数列执行插入排序
+            array[j] = array[j - index];
+            j -= index;
+            array[j] = temp;
+          }
+        }
+        index = Math.floor(index / 2);//步长减半
+      }
     return array
 }
