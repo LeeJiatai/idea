bc85a6dddd9c10581fd241cee9d1c9d8e707cd5a
lijiatai <lijiataihpu@gmail.com>
2019-07-02 21:51:56 +0800

    添加注释:pencil:

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*获取数据类型*/
+/*获取精准数据类型*/
 let dataType = (data) => {
 	let string = Object.prototype.toString.call(data).slice();
 	let type = string.slice(string.indexOf(' ') + 1, string.length - 1).toLowerCase();
